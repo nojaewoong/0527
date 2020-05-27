57ab1196181043886b50f16f12f4ebcf87d6f422
nojaewoong <55523998+nojaewoong@users.noreply.github.com>
2020-05-27 11:02:50 +0900

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void hello()
+{
+	printf("%d",n);
+	if(n==1)
+    return 1;
+
+    factoria(n-1);
+    ptintf("%d",n)
+    
+    
+}
+
+int main()
+{
+    printf("%d",factorial(5));
+
+    return 0;
+}
